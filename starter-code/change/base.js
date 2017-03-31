bdb50a66505e02c1604a8adba9112dab4bdd214b
melissa pringle <melissa.a.acree@gmail.com>
2017-03-30 21:09:01 -0500

    change

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -1,7 +1,23 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
   $("#time").text( Date.now() );
 
-})
+  $('#left').on('change', function() {
+    var lnum=Number($('#left').val());
+    var rnum=Number($('#right').val());
+    var total= lnum+rnum;
+    $('#total').val(total);
+  });
+  $('#right').on('change', function() {
+    var lnum=Number($('#left').val());
+    var rnum=Number($('#right').val());
+    var total= lnum+rnum;
+    $('#total').val(total);
+  });
+  $('#reset').on('click', function() {
+    $('#total').val('');
+    $('#left').val('');
+    $('#right').val('');
+  });
+});
