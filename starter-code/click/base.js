514ed0fd1e0fa456b104743f9b9bb087e064e881
melissa pringle <melissa.a.acree@gmail.com>
2017-03-30 21:03:41 -0500

    click

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,7 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-})
+  $('#imperatives span').on('click', function(){
+    $('ul').append('<li>You clicked: '+$(this).text()+' at '+Date.now()+'</li>');
+  });
+});
